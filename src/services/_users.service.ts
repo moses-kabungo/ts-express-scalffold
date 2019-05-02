b803eb06a13e8f75f2925e7aa080e403376b10df
Moze Kabungo <kbng.moses@gmail.com>
2019-05-02 07:46:59 +0300

    add extra methods on users service interface

--- src/services/_users.service.ts
+++ src/services/_users.service.ts
@@ -1,6 +1,11 @@
 
 import { User } from '../models';
+import { PaginationInfo } from '../middlewares/paginator';
+import { Page } from '../models/_page.model';
 
 export interface IUsersService {
-    create(user: User): Promise<string|User>;
+    create(user: User): Promise<string | User>;
+    count(): Promise<number>;
+    findById(id: string): Promise<User | undefined>;
+    findPage(pageInfo: PaginationInfo): Promise<Page<User>>
 }
