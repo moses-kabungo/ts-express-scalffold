e5d5b322fdf26cf84cffea12631aab1099996bc3
Moze Kabungo <kbng.moses@gmail.com>
2019-05-06 22:30:31 +0300

    Add extra methods on users service

--- src/services/_users.service.ts
+++ src/services/_users.service.ts
@@ -1,11 +1,20 @@
+import Bluebird from 'bluebird';
 
 import { User } from '../models';
-import { PaginationInfo } from '../middlewares/paginator';
+import { PaginationInfo } from '../middlewares/_paginator.middleware';
 import { Page } from '../models/_page.model';
+import { LoginResponse } from '../models/_login-response.model';
+
+
+global.Promise = Bluebird;
 
 export interface IUsersService {
-    create(user: User): Promise<string | User>;
-    count(): Promise<number>;
-    findById(id: string): Promise<User | undefined>;
-    findPage(pageInfo: PaginationInfo): Promise<Page<User>>
+    create(user: User): Bluebird<string|User>;
+    count(): Bluebird<number>;
+    jwtEncode(user: User): Bluebird<string>;
+    jwtDecode(token: string): Bluebird<User|undefined>;
+    findById(id: string): Bluebird<User|undefined>;
+    findPage(pageInfo: PaginationInfo): Bluebird<Page<User>>;
+    login(loginId: string, password: string): Bluebird<LoginResponse>;
+    logout(loginId: string): Bluebird<boolean>;
 }
