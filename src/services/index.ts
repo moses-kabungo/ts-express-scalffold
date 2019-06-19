8170ac8c55fabd30a32a2779339dfda61495c8cb
Moses Kabungo <kbng.moses@gmail.com>
2019-06-19 12:16:09 +0300

    Remove unnecessary exports

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,4 @@
 
-export * from './_abstract-users-service';
 export * from './_cache.service';
 export * from './_token-decoder';
 export * from './_token-encoder';
-export * from './_users.service';
